a5a541e8fdc4c49603f3d1ef619b7b8d26cd29ec
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-07-30 14:47:18 -0600

    header site finished

--- header.php
+++ header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The header for our theme
  *
@@ -12,8 +13,9 @@
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
+
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
@@ -21,39 +23,60 @@
 </head>
 
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'elite_estates' ); ?></a>
+	<?php wp_body_open(); ?>
+	<div id="page" class="site">
+		<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e('Skip to content', 'elite_estates'); ?></a>
 
-	<header id="masthead" class="site-header container">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$elite_estates_description = get_bloginfo( 'description', 'display' );
-			if ( $elite_estates_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $elite_estates_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+		<header id="masthead" class="site-header">
+			<div class="header-bar container">
+				<div class="site-branding">
+					<?php
+					the_custom_logo();
+					if (is_front_page() && is_home()) :
+					?>
+						<h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
+					<?php
+					else :
+					?>
+						<p class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></p>
+					<?php
+					endif;
+					$elite_estates_description = get_bloginfo('description', 'display');
+					if ($elite_estates_description || is_customize_preview()) :
+					?>
+						<p class="site-description"><?php echo $elite_estates_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped 
+																				?></p>
+					<?php endif; ?>
+				</div><!-- .site-branding -->
+
+				<nav id="site-navigation" class="main-navigation">
+					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
+						<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-menu-2" width="52" height="52" viewBox="0 0 24 24" stroke-width="2" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+							<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+							<path d="M4 6l16 0" />
+							<path d="M4 12l16 0" />
+							<path d="M4 18l16 0" />
+						</svg>
+					</button>
+					<?php
+					wp_nav_menu(
+						array(
+							'theme_location' => 'menu-1',
+							'menu_id'        => 'primary-menu',
+						)
+					);
+					?>
+				</nav><!-- #site-navigation -->
+			</div>
+
+			<?php if (is_front_page()) : ?>
+				<div class="container">
+					<div class="header-content">
+						<h1 class="header-heading">Encuentra tu casa de lujo al mejor precio</h1>
+
+						<!-- TODO: Buscador -->
+
+					</div>
+				</div>
 			<?php endif; ?>
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'elite_estates' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+		</header><!-- #masthead -->
