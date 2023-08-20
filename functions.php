818615e359f39debc2422ac15683311e54152770
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-08-20 15:11:41 -0600

    Add: queries.php

--- functions.php
+++ functions.php
@@ -177,3 +177,7 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
+/**
+ * Queries.
+ */
+require get_template_directory() . '/inc/queries.php';
