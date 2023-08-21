9ded32d9fb7e2136a6115ca21afe1464d088e392
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-08-20 18:08:14 -0600

    Created query: list properties

--- inc/queries.php
+++ inc/queries.php
@@ -1,2 +1,50 @@
-<?php 
+<?php
 
+function elite_estates_list_properties($cantidad = -1)
+{
+?>
+  <div class="properties-listing">
+    <?php
+    $args = array(
+      'post_type' => 'properties',
+      'posts_per_page' => $cantidad
+    );
+
+    $properties = new WP_Query($args);
+
+    while ($properties->have_posts()) {
+      $properties->the_post(); ?>
+      <?php
+      $price = intval(get_field('price'));
+      ?>
+
+      <div class="property">
+        <div class="property-image">
+          <a href="<?php the_permalink() ?>">
+            <?php the_post_thumbnail() ?>
+          </a>
+        </div>
+
+        <div class="property-content">
+          <div class="property-info">
+            <p class="property-city"><?php echo esc_html(get_field('city')); ?></p>
+            <p class="property-type"><?php echo esc_html(get_field('rent_or_buy')); ?></p>
+          </div>
+          <a href="<?php the_permalink() ?>">
+            <h3 class="property-title"><?php the_title() ?></h3>
+          </a>
+          <p class="property-price">MXN <?php echo esc_html(number_format($price)); ?></p>
+          <ul class="property-meta">
+            <li class="property-beds"><span><?php echo esc_html(get_field('bedroom')); ?></span> Rec.</li>
+            <li class="property-baths"><span><?php echo esc_html(get_field('bathroom')); ?></span> Baño(s)</li>
+            <li class="property-parking"><span><?php echo esc_html(get_field('parking')); ?></span> Estac.</li>
+          </ul>
+          <p class="property-location"><?php echo esc_html(get_field('address')); ?></p>
+        </div>
+      </div>
+    <?php };
+    wp_reset_postdata(); // Volver a su estado original la clase de WordPress ya que se modificó previamente
+    ?>
+  </div>
+<?php
+};
